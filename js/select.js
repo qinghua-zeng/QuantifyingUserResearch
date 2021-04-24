3dec81f21dfb44aa043a84face09e682e54515e8
qinghua-zeng <859066288@qq.com>
2021-04-24 17:02:39 +1200

    First Commit

---
+++ js/select.js
@@ -0,0 +1,165 @@
+
+
+
+var situation = 0;
+$("#parent *").click(function (e) {
+	if (e.target == $("#a1")[0]) {
+		//alert("你点了链接一！");
+		if (situation == 0) {
+			$("#a0").text("比较数据还是相关数据");
+			$("#a1").text("yes");
+			$("#a2").text("no");
+			situation = 1;
+		}
+
+		else if (situation == 1) {
+			$("#a0").text("比较数据");
+			$("#a1").text("yes");
+			$("#a2").text("no");
+			situation = 2;
+		}
+
+		else if (situation == 2) {
+			$("#a0").text("每组用户不同吗");
+			$("#a1").text("yes");
+			$("#a2").text("no");
+			situation = 3;
+		}
+
+		else if (situation == 3) {
+			$("#a0").text("3组或更多组");
+			$("#a1").text("yes");
+			$("#a2").text("no");
+			situation = 4;
+		}
+
+		else if (situation == 4) {
+			$("#a0").text("3组或更多组");
+			$("#a1").text("方差分析或多重样本t检验");
+			$("#a2").text("");
+			
+		}
+
+		else if (situation == 5) {
+			$("#a0").text("有任务时间？");
+			$("#a1").text("Yes");
+			$("#a2").text("no");
+			situation = 8;
+		}
+
+		else if (situation == 7) {
+			$("#a0").text("大于等于3组");
+			$("#a1").text("方差分析或多重配对t检验");
+			$("#a2").text("");
+			situation = 7;
+		}
+
+		else if (situation == 8) {
+			$("#a0").text("有任务时间？");
+			$("#a1").text("单样本t检验（log）");
+			$("#a2").text("");
+			situation = 8;
+		}
+
+		else if (situation == 9) {
+			$("#a0").text("有任务时间？");
+			$("#a1").text("t(log）或中位数置信区间");
+			$("#a2").text("");
+			situation = 8;
+		}
+
+		else if (situation == 10) {
+			$("#a0").text("比较数据");
+			$("#a1").text("yes");
+			$("#a2").text("no");
+			situation = 11;
+		}
+
+		else if (situation == 11) {
+			$("#a0").text("每组用户不同吗");
+			$("#a1").text("yes");
+			$("#a2").text("no");
+			situation = 12;
+		}
+
+		else if (situation == 12) {
+			$("#a0").text("3组或更多组");
+			$("#a1").text("yes");
+			$("#a2").text("no");
+			situation = 13;
+		}
+
+		else if (situation == 13) {
+			$("#a0").text("大于等于3组");
+			$("#a1").text("卡方检验或比例差异的wald校正");
+			$("#a2").text("");
+			situation = 13;
+		}
+
+		else if (situation == 14) {
+			$("#a0").text("是的，与基准进行比较检验");
+			$("#a1").text("单样本二项式或单样本z检验");
+			$("#a2").text("");
+			situation = 14;
+		}
+
+		else if (situation == 16) {
+			$("#a0").text("大于等于3组");
+			$("#a1").text("配对比例差异的wald校正置信区间");
+			$("#a2").text("");
+			situation = 16;
+		}
+
+
+//no
+	} else if (e.target == $("#a2")[0]) {
+		
+		if (situation == 0) {
+			$("#a0").text("比较数据还是相关数据");
+			$("#a1").text("yes");
+			$("#a2").text("no");
+			situation = 10;
+		}
+
+		if (situation == 1) {
+			$("#a0").text("与基准进行比较检验");
+			$("#a1").text("yes");
+			$("#a2").text("no");
+			situation = 5;
+		}
+
+		if (situation == 2) {
+			$("#a0").text("比较数据");
+			$("#a1").text("相关性分析和回归分析");
+			$("#a2").text("");
+			situation = 2;
+		}
+
+		if (situation == 3) {
+			$("#a0").text("3组或更多组");
+			$("#a1").text("yes");
+			$("#a2").text("no");
+			situation = 7;
+		}
+		
+		else if (situation == 4) {
+			$("#a0").text("小于3组");
+			$("#a1").text("双样本t检验");
+			$("#a2").text("");
+		}
+		
+		else if (situation == 5) {
+			$("#a0").text("有任务时间？");
+			$("#a1").text("yes");
+			$("#a2").text("no");
+			situation = 9;
+		}
+
+		else if (situation == 7) {
+			$("#a0").text("小于3组");
+			$("#a1").text("配对t检验");
+			$("#a2").text("");
+			situation = 9;
+		}
+	}
+});
