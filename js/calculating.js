3e78a4220e3fc822627767b782af34ecc3e34c5b
qinghua-zeng <859066288@qq.com>
2021-05-22 14:28:16 +1200

    First Commit

--- js/calculating.js
+++ js/calculating.js
@@ -125,12 +125,12 @@ $(document).ready(function () {//传说中的ready
 
 		this.q10meanDiff = this.Stat1.mean - this.Stat2.mean;
 
-		this.q10s1=this.Stat1.stddev;
-		this.q10s2=this.Stat2.stddev;
-		this.q10belowLeft=Math.sqrt(this.Stat1.stddev * this.Stat1.stddev/this.Stat1.sampleSize);
-		this.q10belowRight=Math.sqrt(this.Stat2.stddev * this.Stat2.stddev/this.Stat2.sampleSize);
+		this.q10s1 = this.Stat1.stddev;
+		this.q10s2 = this.Stat2.stddev;
+		this.q10belowLeft = Math.sqrt(this.Stat1.stddev * this.Stat1.stddev / this.Stat1.sampleSize);
+		this.q10belowRight = Math.sqrt(this.Stat2.stddev * this.Stat2.stddev / this.Stat2.sampleSize);
 
-		this.q10t=this.q10meanDiff/(this.q10belowLeft+this.q10belowRight);
+		this.q10t = this.q10meanDiff / (this.q10belowLeft + this.q10belowRight);
 		//this.q10below = this.Stat1.stddev * this.Stat1.stddev/this.Stat1.sampleSize - this.Stat2.stddev * this.Stat2.stddev/this.Stat2.sampleSize;
 		$("#q10s1").val(this.q10s1);
 		$("#q10s2").val(this.q10s2);
@@ -146,119 +146,238 @@ $(document).ready(function () {//传说中的ready
 		this.q11G2SuccessNum = parseFloat($("#q11G2SuccessNum").val());
 		this.q11G2Total = parseFloat($("#q11G2Total").val());
 
-		this.q11P=(this.q11G1SuccessNum+this.q11G2SuccessNum)/(this.q11G1Total+this.q11G2Total);
-		this.q11PQ=this.q11P*(1-this.q11P);
-		this.q11SuccessRate1=this.q11G1SuccessNum/this.q11G1Total;
-		this.q11SuccessRate2=this.q11G2SuccessNum/this.q11G2Total;
+		this.q11P = (this.q11G1SuccessNum + this.q11G2SuccessNum) / (this.q11G1Total + this.q11G2Total);
+		this.q11PQ = this.q11P * (1 - this.q11P);
+		this.q11SuccessRate1 = this.q11G1SuccessNum / this.q11G1Total;
+		this.q11SuccessRate2 = this.q11G2SuccessNum / this.q11G2Total;
 
-		this.q11totalSample=this.q11G1Total+this.q11G2Total;
+		this.q11totalSample = this.q11G1Total + this.q11G2Total;
 		//this.q11z=(this.q11SuccessRate1-this.q11SuccessRate2)*Math.sqrt((this.q11totalSample-1)/this.q11G1Total+this.q11G2Total)/Math.sqrt(this.q11PQ*(1/this.q11G1SuccessNum+1/this.q11G2SuccessNum));
-		this.upLeft=this.q11SuccessRate1-this.q11SuccessRate2;
-		this.upRight=Math.sqrt((this.q11totalSample-1)/this.q11totalSample);
-		this.dowmRight=1/this.q11G1Total+1/this.q11G2Total;
+		this.upLeft = this.q11SuccessRate1 - this.q11SuccessRate2;
+		this.upRight = Math.sqrt((this.q11totalSample - 1) / this.q11totalSample);
+		this.dowmRight = 1 / this.q11G1Total + 1 / this.q11G2Total;
 		//window.alert(this.upLeft);
 		//window.alert(this.upRight);
 		//window.alert(this.q11PQ);
 		//window.alert(this.dowmRight);
-		this.q11z=this.upLeft*this.upRight/Math.sqrt(this.q11PQ*this.dowmRight);
-		this.q11zAbs=Math.abs(this.q11z);//取绝对值
+		this.q11z = this.upLeft * this.upRight / Math.sqrt(this.q11PQ * this.dowmRight);
+		this.q11zAbs = Math.abs(this.q11z);//取绝对值
 		$("#q11z").val(this.q11zAbs);
 	});
 
 	//==============12 McNemar 精确检验（5）====================
 	$("#Caluclate_McNemarExactTest").click(function () {
+		this.q12V1 = $("#q12V1").val().split(",");
+		this.q12V2 = $("#q12V2").val().split(",");
+
+		//this.q12a=0;
+		this.q12b = 0;
+		this.q12c = 0;
+		//this.q12d=0;
+
+		for (let i = 0; i < this.q12V1.length; i++) {
+			if (this.q12V1[i] == 1 && this.q12V2[i] == 0) { this.q12b++; }
+			if (this.q12V1[i] == 0 && this.q12V2[i] == 1) { this.q12c++; }
+		}
+
+		//从b或c中选择较小的那个值
+		if (this.q12b > this.q12c) {
+			this.q12selected = this.q12c;
+		} else {
+			this.q12selected = this.q12b;
+		}
+		this.q12n = this.q12b + this.q12c//异序对的总数
+
+		/* calP=function(x){
+			this.p=factorial(this.q12n)/factorial(x)*factorial(this.q12n-x)*Math.pow(0.5,x)*Math.pow(1-0.5,this.q12n-x);
+			return this.p;
+		} */
+
+		this.q12p = calP(this.q12selected, this.q12n);//观测值的概率
+
+		this.q12lessP = 0;
+		for (let i = 0; i < this.q12selected; i++) {
+			this.q12lessP += calP(i, this.q12n);
+		}
+		this.q12MidP = this.q12lessP + this.q12p / 2;//单侧检验的中间p值
+		//this.q12MidP=this.q12p;
+		$("#q12b").val(this.q12b);
+		$("#q12c").val(this.q12c);
+		$("#q12SingleP").val(this.q12MidP);
+		$("#q12DoubleP").val(this.q12MidP * 2);
+
+
 
 	});
 
-	//================13 方差分析或多重配对t检验（5.2.1）====================
-	$("#Caluclate_AnovaOrMultiplePairedT").click(function () {
-
-	});
-
-	//================14 方差分析或多重双样本t检验（5.2.3）====================
-	$("#Caluclate_AnovaOrMultipleTwoSampleT").click(function () {
-
-	});
-
-	//==============15 卡方（11）===================
-	$("#Caluclate_ChiSquare").click(function () {
-
-	});
-
-	//===============16 比例差异的Wald校正（11）====================
-	$("#Caluclate_AdjustedWaldDifferenceInProportion").click(function () {
-
-	});
-
-	//=================17 配对比例差异的Wald校正置信区间（5）====================
-	$("#Caluclate_AdjustedWaldConfidenceIntervalForDifferenceInMatchedProportions").click(function () {
-
-	});
-
-	//===============18 相关性（10）===================
-	$("#Caluclate_Correlation").click(function () {
-
-	});
-
-	//================19 回归分析（10）====================
-	$("#Caluclate_RegressionAnalysis").click(function () {
-
-	});
-
-	//================20 Phi相关性====================
-	$("#Caluclate_Phi").click(function () {
-
-	});
-
-	//================21 平均值误差的范围（6）====================
+	//================13 平均值误差的范围（6）====================
 	$("#Caluclate_MarginOfErrorMean").click(function () {
+		this.q13s = parseFloat($("#q13s").val());
+		this.q13d = parseFloat($("#q13d").val());
+		this.q13Expectation = parseFloat($("#q13Expectation").val());
 
+		
+		
+		this.q13z = getT(this.q13Expectation, 10000);//传入一个大于1000的数计算t95 inf
+		//this.q13n=Math.pow(this.q13z,2)*Math.pow(this.q13s,2)/Math.pow(this.q13d,2);
+
+		this.q13n = Math.pow(this.q13z, 2) * this.q13s * this.q13s / (this.q13d * this.q13d);
+
+		this.q13arrayN = new Array();
+		this.q13arrayN[0] = calN(this.q13s, this.q13d, this.q13z);//
+		this.q13z = getT(this.q13Expectation, this.q13arrayN[0] - 1);//2.093
+		this.q13arrayN[1] = calN(this.q13s, this.q13d, this.q13z);
+		this.q13z = getT(this.q13Expectation, this.q13arrayN[1] - 1);//2.08
+
+		for (let i = 0; this.q13arrayN[i] !== this.q13arrayN[i + 1]; i++) {
+			this.q13arrayN[i + 2] = calN(this.q13s, this.q13d, this.q13z);
+			//window.alert(this.q13arrayN[i+1]);
+			this.q13z = getT(this.q13Expectation, this.q13arrayN[i + 2] - 1);
+
+		}
+
+
+
+
+		$("#q13t").val(this.q13z);
+		$("#q13n").val(this.q13arrayN);
 	});
 
-	//================22 比例误差的幅度（6）====================
-	$("#Caluclate_MarginOfErrorProportion").click(function () {
-
-	});
-
-	//================23 均值与基准比较（6）====================
+	//================14 大样本的二项样本量估计（6.5）====================
 	$("#Caluclate_MeantoCriterion").click(function () {
+		this.q14p = parseFloat($("#q14p").val());
+		this.q14d = parseFloat($("#q14d").val());
+		this.q14confi = parseFloat($("#q14confi").val());
 
+		this.q14z= getT(this.q14confi,10000);
+		this.q14n=this.q14z*this.q14z*this.q14p*(1-this.q14p)/(this.q14d*this.q14d);
+		
+
+		$("#q14n").val(this.q14n);
 	});
 
-	//================24 比例与基准比较（6）====================
-	$("#Caluclate_ProportionToCriterion").click(function () {
+//================24 比例与基准比较（6）====================
+$("#Caluclate_ProportionToCriterion").click(function () {
+	this.q15s = parseFloat($("#q15s").val());
+	this.q15d = parseFloat($("#q15d").val());
+	this.q15Expectation = parseFloat($("#q15Expectation").val());
+	this.q15b = parseFloat($("#q15b").val());
 
-	});
+	
+	
+	this.q15z = getT(this.q15b, 10000);//传入一个大于1000的数计算t95 inf
+	//this.q13n=Math.pow(this.q13z,2)*Math.pow(this.q13s,2)/Math.pow(this.q13d,2);
+	this.q15zb = getT(this.q15Expectation, 10000);
+	this.q15n = Math.pow(this.q15z, 2) * this.q15s * this.q15s / (this.q15d * this.q15d);
 
-	//================25 双均数（6）====================
-	$("#Caluclate_TwoMeans").click(function () {
+	this.q15arrayN = new Array();
+	this.q15arrayN[0] = calN(this.q15s, this.q15d, this.q15z+this.q15zb);//
+	this.q15z = getT(this.q15Expectation, this.q15arrayN[0] - 1);//2.093
+	this.q15zb = getT(this.q15b, this.q15arrayN[0] - 1);
+	this.q15arrayN[1] = calN(this.q15s, this.q15d, this.q15z+this.q15zb);
+	this.q15z = getT(this.q15Expectation, this.q15arrayN[1] - 1);//2.08
+	this.q15zb = getT(this.q15b, this.q15arrayN[1] - 1);//2.08
 
-	});
+	for (let i = 0; this.q15arrayN[i] !== this.q15arrayN[i + 1]; i++) {
+		this.q15arrayN[i + 2] = calN(this.q15s, this.q15d, this.q15z+this.q15zb);
+		//window.alert(this.q13arrayN[i+1]);
+		this.q15z = getT(this.q15Expectation, this.q15arrayN[i + 2] - 1);
+		this.q15zb = getT(this.q15b, this.q15arrayN[i + 2] - 1);
 
-	//================26 配对均数（6）===================
-	$("#Caluclate_PairedMeans").click(function () {
+	}
 
-	});
 
-	//================27 双比例（6）====================
-	$("#Caluclate_TwoProportions").click(function () {
 
-	});
 
-	//=================28 配对比例（6）====================
-	$("#Caluclate_PairedProportions").click(function () {
+	$("#q15t").val(this.q15z);
+	$("#q15n").val(this.q15arrayN);
+});
 
-	});
+	//未编辑代码
+	{
 
-	//================29 线性回归的样本量===================
-	$("#Caluclate_SampleSizeForLinearRegression").click(function () {
+		//================13 方差分析或多重配对t检验（5.2.1）====================
+		$("#Caluclate_AnovaOrMultiplePairedT").click(function () {
 
-	});
+		});
 
-	//=================30 问题发现的样本量====================
-	$("#Caluclate_ProblemDiscoverySampleSize").click(function () {
+		//================14 方差分析或多重双样本t检验（5.2.3）====================
+		$("#Caluclate_AnovaOrMultipleTwoSampleT").click(function () {
 
-	});
+		});
+
+		//==============15 卡方（11）===================
+		$("#Caluclate_ChiSquare").click(function () {
+
+		});
+
+		//===============16 比例差异的Wald校正（11）====================
+		$("#Caluclate_AdjustedWaldDifferenceInProportion").click(function () {
+
+		});
+
+		//=================17 配对比例差异的Wald校正置信区间（5）====================
+		$("#Caluclate_AdjustedWaldConfidenceIntervalForDifferenceInMatchedProportions").click(function () {
+
+		});
+
+		//===============18 相关性（10）===================
+		$("#Caluclate_Correlation").click(function () {
+
+		});
+
+		//================19 回归分析（10）====================
+		$("#Caluclate_RegressionAnalysis").click(function () {
+
+		});
+
+		//================20 Phi相关性====================
+		$("#Caluclate_Phi").click(function () {
+
+		});
+
+
+
+		//================22 比例误差的幅度（6）====================
+		$("#Caluclate_MarginOfErrorProportion").click(function () {
+
+		});
+
+
+
+		
+
+		//================25 双均数（6）====================
+		$("#Caluclate_TwoMeans").click(function () {
+
+		});
+
+		//================26 配对均数（6）===================
+		$("#Caluclate_PairedMeans").click(function () {
+
+		});
+
+		//================27 双比例（6）====================
+		$("#Caluclate_TwoProportions").click(function () {
+
+		});
+
+		//=================28 配对比例（6）====================
+		$("#Caluclate_PairedProportions").click(function () {
+
+		});
+
+		//================29 线性回归的样本量===================
+		$("#Caluclate_SampleSizeForLinearRegression").click(function () {
+
+		});
+
+		//=================30 问题发现的样本量====================
+		$("#Caluclate_ProblemDiscoverySampleSize").click(function () {
+
+		});
+	}
+
 
 
 	//在这上面放代码	
--- js/simpleCal.js
+++ js/simpleCal.js
@@ -1,55 +1,95 @@
-var t95 = [12.71, 4.3, 3.18, 2.78, 2.57, 2.45, 2.36, 2.31, 2.26, 2.23, 2.2, 2.18, 2.16, 2.14, 2.13];
+//t table
+{
+	//80%置信度的t表
+	{
+		var t80 = [1.376,1.061,0.978,0.941,0.920,0.906,0.896,0.889,0.883,0.879,0.876,0.873,0.870,0.868,0.866,0.865,0.863,0.862,0.861,0.860,0.859,0.858,0.858,0.857,0.856,0.856,0.855,0.855,0.854,0.854];
+		//window.alert(t80.length);
+		var t80_60 = 0.848;
+		var t80_100 = 0.845;
+		var t80_1000 = 0.842;
+		var t80_inf = 0.842;
+	}
+
+	//90%置信度的t表
+	{
+		var t90 = [6.314, 2.92, 2.353, 2.132, 2.015, 1.943, 1.895, 1.86, 1.833, 1.812, 1.796, 1.782, 1.771, 1.761, 1.753, 1.746, 1.74, 1.734, 1.729, 1.725, 1.721, 1.717, 1.714, 1.711, 1.708, 1.706, 1.703, 1.701, 1.699, 1.697];
+		var t90_60 = 1.671;
+		var t90_120 = 1.658;
+		var t90_1000 = 1.646;
+		var t90_inf = 1.645;
+	}
+
+	//95%置信度的t表
+	{
+		var t95 = [12.71, 4.3, 3.18, 2.78, 2.57, 2.45, 2.36, 2.31, 2.26, 2.23, 2.2, 2.18, 2.16, 2.14, 2.13, 2.12, 2.11, 2.101, 2.093, 2.086, 2.08, 2.074, 2.069, 2.064, 2.06, 2.056, 2.052, 2.048, 2.045, 2.042];//前30个值
+
+		var t95_60 = 2;
+		var t95_120 = 1.98;
+		var t95_1000 = 1.962;
+		var t95_inf = 1.96;
+	}
+
+	//99%置信度的t表
+
+	{
+		var t99 = [63.656, 9.925, 5.841, 4.604, 4.032, 3.707, 3.499, 3.355, 3.25, 3.169, 3.106, 3.055, 3.012, 2.977, 2.947, 2.921, 2.898, 2.878, 2.861, 2.845, 2.831, 2.819, 2.807, 2.797, 2.787, 2.779, 2.771, 2.763, 2.756, 2.75]
+		var t99_60 = 2.66;
+		var t99_120 = 2.617;
+		var t99_1000 = 2.581;
+		var t99_inf = 2.576;
+	}
+}
 
 //数组内数值求和
 sumAll2 = function (arr) {
-    this.sum2 = 0;
-    for (let i = 0; i < arr.length; i++) {
-        this.sum2 += arr[i];
-    }
-    //window.alert(this.sum2);
-    return this.sum2;
+	this.sum2 = 0;
+	for (let i = 0; i < arr.length; i++) {
+		this.sum2 += arr[i];
+	}
+	//window.alert(this.sum2);
+	return this.sum2;
 }
 
 //数组内与平均数的偏离
-calDev = function (ar){
-    //this.arr=[0,1];
-    this.devs=new Array();
-    for (let i = 0; i < ar.length; i++) {
-        this.devs[i] = ar[i] - sumAll2(ar)/ar.length;
-    }
-    //window.alert(this.devs);
-    return this.devs;
+calDev = function (ar) {
+	//this.arr=[0,1];
+	this.devs = new Array();
+	for (let i = 0; i < ar.length; i++) {
+		this.devs[i] = ar[i] - sumAll2(ar) / ar.length;
+	}
+	//window.alert(this.devs);
+	return this.devs;
 }
 
 //样本标准差
-calStdDev = function (arrr){
-    
-    this.deviations=calDev(arrr);
-    this.temp = 0;
+calStdDev = function (arrr) {
+
+	this.deviations = calDev(arrr);
+	this.temp = 0;
 	for (let i = 0; i < this.deviations.length; i++) {
 		this.temp += this.deviations[i] * this.deviations[i];
 	}
-    this.stdDev = Math.sqrt(this.temp / (arrr.length - 1));
-    //window.alert(this.stdDev);
-    return this.stdDev;
+	this.stdDev = Math.sqrt(this.temp / (arrr.length - 1));
+	//window.alert(this.stdDev);
+	return this.stdDev;
 
 }
 
 //计算数组的时间对数
-calNumlog = function (arr){
-    this.Logs = new Array();
+calNumlog = function (arr) {
+	this.Logs = new Array();
 	for (let i = 0; i < arr.length; i++) {
 		this.Logs[i] = Math.log(arr[i]);
 	}
 	//求和
 	//this.logSum=sumAll2(this.Logs);
-    //window.alert(this.Logs);
-    return this.Logs;
+	//window.alert(this.Logs);
+	return this.Logs;
 
 }
 
 //中位数
-median =function (args) {
+median = function (args) {
 	//let args=[1,2,3,3,6]; //收集参数转为数组
 	args.sort(function (a, b) { return a - b }) //排序
 	//return args;
@@ -61,7 +101,7 @@ median =function (args) {
 }
 
 //总体标准偏差的方差(PSD)
- varianceArr=function(arr) {
+varianceArr = function (arr) {
 	let s,
 		ave,
 		sum = 0,
@@ -79,13 +119,94 @@ median =function (args) {
 };
 
 //阶乘
-factorial= function(num){
-    if(num <= 1){
-        return 1;
-    }else{
-        return num * factorial(num - 1);
-    }
+factorial = function (num) {
+	if (num <= 1) {
+		return 1;
+	} else {
+		return num * factorial(num - 1);
+	}
 }
 
-//var sss=factorial(3);
-//window.alert(sss);
+
+//McNemar p值计算函数
+calP = function (x, n) {
+	this.p = factorial(n) / (factorial(x) * factorial(n - x)) * Math.pow(0.5, x) * Math.pow(1 - 0.5, n - x);
+	return this.p;
+}
+
+//window.alert(calP(0,7));
+
+
+//q13 计算N
+calN = function (s, d, z) {
+	this.n = Math.pow(z, 2) * s * s / (d * d);
+	this.n = Math.ceil(this.n);//向上取整
+	return this.n;
+
+}
+
+getT = function (p, n) {
+	//this.t=t95[n];
+	if (p == 80) {
+		if (n <= 30) {
+			this.t = t80[n - 1];
+		} else if (n > 30 && n <= 60) {
+			this.t = t80_60;
+		} else if (n > 60 && n <= 100) {
+			this.t = t80_100;
+		} else if (n > 100 && n <= 1000) {
+			this.t = t80_1000;
+		} else {
+			this.t = t80_inf;
+		}
+
+	}
+
+	if (p == 90) {
+		if (n <= 30) {
+			this.t = t90[n - 1];
+		} else if (n > 30 && n <= 60) {
+			this.t = t90_60;
+		} else if (n > 60 && n <= 120) {
+			this.t = t90_120;
+		} else if (n > 120 && n <= 1000) {
+			this.t = t90_1000;
+		} else {
+			this.t = t90_inf;
+		}
+
+	}
+
+	if (p == 95) {
+		if (n <= 30) {
+			this.t = t95[n - 1];
+		} else if (n > 30 && n <= 60) {
+			this.t = t95_60;
+		} else if (n > 60 && n <= 120) {
+			this.t = t95_120;
+		} else if (n > 120 && n <= 1000) {
+			this.t = t95_1000;
+		} else {
+			this.t = t95_inf;
+		}
+
+	}
+
+	if (p == 99) {
+		if (n <= 30) {
+			this.t = t99[n - 1];
+		} else if (n > 30 && n <= 60) {
+			this.t = t99_60;
+		} else if (n > 60 && n <= 120) {
+			this.t = t99_120;
+		} else if (n > 120 && n <= 1000) {
+			this.t = t99_1000;
+		} else {
+			this.t = t99_inf;
+		}
+
+	}
+
+	return this.t;
+}
+
