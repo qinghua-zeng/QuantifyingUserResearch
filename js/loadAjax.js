6f0a63e9cbde12261181ec292774e487f98f7a5b
qinghua-zeng <859066288@qq.com>
2021-08-01 22:46:16 +1200

    First Commit

--- js/loadAjax.js
+++ js/loadAjax.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
    // $("#div2").load("head_Math_js.html");
     
     $("button").click(function () {
-        $("#div1").load("statistics/02.html");
+       // $("#div1").load("statistics/02.html");
     });
 
   
