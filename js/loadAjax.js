507fa14f594d08c5fa525bfadc4ed3cc6946b9c3
qinghua-zeng <859066288@qq.com>
2021-08-01 23:12:42 +1200

    First Commit

--- js/loadAjax.js
+++ js/loadAjax.js
@@ -1,12 +1,84 @@
 $(document).ready(function () {
 
-   // $("#div2").load("head_Math_js.html");
-    
-    $("button").click(function () {
-       // $("#div1").load("statistics/02.html");
+
+
+    $("#q01").click(function(){
+      $("#div1").load("statistics/01.html");
     });
 
-  
+    $("#q02").click(function(){
+      $("#div1").load("statistics/02.html");
+    });
 
+    $("#q03").click(function(){
+      $("#div1").load("statistics/03.html");
+    });
 
+    $("#q04").click(function(){
+      $("#div1").load("statistics/04.html");
+    });
+
+    $("#q05").click(function(){
+      $("#div1").load("statistics/05.html");
+    });
+
+    $("#q06").click(function(){
+      $("#div1").load("statistics/06.html");
+    });
+
+    $("#q07").click(function(){
+      $("#div1").load("statistics/07.html");
+    });
+
+    $("#q08").click(function(){
+      $("#div1").load("statistics/08.html");
+    });
+
+    $("#q09").click(function(){
+      $("#div1").load("statistics/09.html");
+    });
+
+    $("#q10").click(function(){
+      $("#div1").load("statistics/10.html");
+    });
+
+    $("#q11").click(function(){
+      $("#div1").load("statistics/11.html");
+    });
+
+    $("#q12").click(function(){
+      $("#div1").load("statistics/12.html");
+    });
+
+    $("#q13").click(function(){
+      $("#div1").load("statistics/13.html");
+    });
+
+    $("#q14").click(function(){
+      $("#div1").load("statistics/14.html");
+    });
+
+    $("#q15").click(function(){
+      $("#div1").load("statistics/15.html");
+    });
+
+    $("#q16").click(function(){
+      $("#div1").load("statistics/16.html");
+    });
+
+    $("#q17").click(function(){
+      $("#div1").load("statistics/17.html");
+    });
+
+    $("#q18").click(function(){
+      $("#div1").load("statistics/18.html");
+    });
+
+    $("#q19").click(function(){
+      $("#div1").load("statistics/19.html");
+    });
+
+    $("#q20").click(function(){
+      $("#div1").load("statistics/20.html");
+    });
 });
